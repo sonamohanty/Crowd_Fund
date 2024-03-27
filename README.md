ff05d3445a92db0239108a1ede56f550e44e06af
sonamohanty <90900471+sonamohanty@users.noreply.github.com>
2024-03-27 15:50:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Crowd_Fund
+# CS546-Crowdfunding
+
+Crowdfunding web application for CSe-28 Web Programming class at Parul Institute of Enggeneering and Technology.
+
+### To install the dependencies:
+``` npm install ```
+
+### To run the seed task and populate the database:
+``` npm run seed ```
+
+### To run the server:
+``` npm start ```
---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const static = express.static(__dirname + '/public');
+const configRoutes = require('./routes');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const handlebarsInstance = exphbs.create({
+	defaultLayout: 'main',
+	partialsDir: ['views/partials/']
+});
+
+function configApp(app, i) {
+	app.use(
+		session({
+			name: 'CrowdFundingSession' + i,
+			secret: "ssshhhhh",
+			saveUninitialized: true,
+			resave: false
+		})
+	);
+	app.use('/public', static);
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
+
+	app.engine('handlebars', handlebarsInstance.engine);
+	app.set('view engine', 'handlebars');
+	app.set('views', __dirname + '/views');
+
+	let redirectToSignIn = (req, res, next) => {
+		if(!req.session.user)
+			res.redirect('/users/signin');
+		else
+			next();
+	};
+
+	app.use('/projects/new', redirectToSignIn);
+	app.use('/projects/search', redirectToSignIn);
+	app.use('/projects/edit/:id', redirectToSignIn);
+	app.use('/projects/searchResult', redirectToSignIn);
+	app.use('/projects/deactivate/:id', redirectToSignIn);
+	app.use('/projects/activate/:id', redirectToSignIn);
+
+	app.use('/users/history/:creator', redirectToSignIn);
+
+	configRoutes(app);
+}
+
+configApp(app, 1);
+// configApp(app1, 2);
+
+app.listen(3035, () => {
+	console.log("We've now got a server!");
+	console.log('Your routes will be running on http://localhost:3035');
+});
+
+
